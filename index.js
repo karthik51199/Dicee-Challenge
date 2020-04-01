959e36c6fd1c54b219df903218a41f1b3ededec4
Karthik Paladugula <karthik51199@gmail.com>
2020-04-01 10:11:06 +0530

    Enhanced code in index.js

--- index.js
+++ index.js
@@ -11,42 +11,5 @@ else if (n1 < n2)
 else
   document.querySelector("h1").innerHTML = "Draw !";
 
-switch (n1) {
-  case 1:
-    document.querySelector(".img1").setAttribute("src", "images/dice1.png");
-  break;
-  case 2:
-    document.querySelector(".img1").setAttribute("src", "images/dice2.png");
-  break;
-  case 3:
-    document.querySelector(".img1").setAttribute("src", "images/dice3.png");
-  break;
-  case 4:
-    document.querySelector(".img1").setAttribute("src", "images/dice4.png");
-  break;
-  case 5:
-    document.querySelector(".img1").setAttribute("src", "images/dice5.png");
-  break;
-  case 6:
-    document.querySelector(".img1").setAttribute("src", "images/dice6.png");
-}
-
-switch (n2) {
-  case 1:
-    document.querySelector(".img2").setAttribute("src", "images/dice1.png");
-  break;
-  case 2:
-    document.querySelector(".img2").setAttribute("src", "images/dice2.png");
-  break;
-  case 3:
-    document.querySelector(".img2").setAttribute("src", "images/dice3.png");
-  break;
-  case 4:
-    document.querySelector(".img2").setAttribute("src", "images/dice4.png");
-  break;
-  case 5:
-    document.querySelector(".img2").setAttribute("src", "images/dice5.png");
-  break;
-  case 6:
-    document.querySelector(".img2").setAttribute("src", "images/dice6.png");
-}
+document.querySelector(".img1").setAttribute("src", "images/dice"+n1+".png");
+document.querySelector(".img2").setAttribute("src", "images/dice"+n2+".png");
